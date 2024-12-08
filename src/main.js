42add2b3cdab45618f41775261cbbe086901d2a5
tacheraSasi <tacherasasi@gmail.com>
2024-12-08 14:39:13 +0300

    structured the index .html

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import viteLogo from '/vite.svg'
 import { setupCounter } from './counter.js'
 import { sender } from '../mailer.js'
 
-sender()
+sender(to,subject,message,"From:<ekilirelay> relay@ekilie.com")
 setupCounter(document.querySelector('#counter'))
